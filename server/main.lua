b2784e2fb1d71771233ea8eea3b9c275020e33cd
MaDHouSe79 <madhouse1979@gmail.com>
2022-07-27 00:54:10 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -5,31 +5,31 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local function sendToDiscord(title, message)
-	if Config.UseWebHook then
-		if Config.Webhook == "" then
-			print("you have no webhook, create one on discord [https://discord.com/developers/applications] and place this in the config.lua (Config.Webhook)")
-		else
-			if message == nil or message == '' then return end
-			LogArray = {
-				{
-					["color"] = "16711680",
-					["title"] = "WIPE PLAYER ACCOUNT",
-					["description"] = "Time: **"..os.date('%Y-%m-%d %H:%M:%S').."**",
-					["fields"] = {
-						{
-							["name"] = "Message",
-							["value"] = message
-						}
-					},
-					["footer"] = {
-						["text"] = "qb-wipeplayer by MaDHouSe",
-						["icon_url"] = "https://icons.iconarchive.com/icons/iconarchive/red-orb-alphabet/128/Letter-M-icon.png",
-					}
-				}
+    if Config.UseWebHook then
+	if Config.Webhook == "" then
+	    print("you have no webhook, create one on discord [https://discord.com/developers/applications] and place this in the config.lua (Config.Webhook)")
+	else
+	    if message == nil or message == '' then return end
+		LogArray = {
+		    {
+			["color"] = "16711680",
+			["title"] = "WIPE PLAYER ACCOUNT",
+			["description"] = "Time: **"..os.date('%Y-%m-%d %H:%M:%S').."**",
+			["fields"] = {
+			{
+			    ["name"] = "Message",
+			    ["value"] = message
 			}
-			PerformHttpRequest(Config.Webhook , function(err, text, headers) end, 'POST', json.encode({username = "WIPE PLAYER", embeds = LogArray}), { ['Content-Type'] = 'application/json' })
-		end
+		    },
+			["footer"] = {
+			["text"] = "qb-wipeplayer by MaDHouSe",
+			["icon_url"] = "https://icons.iconarchive.com/icons/iconarchive/red-orb-alphabet/128/Letter-M-icon.png",
+		    }
+		}
+	    }
+	    PerformHttpRequest(Config.Webhook , function(err, text, headers) end, 'POST', json.encode({username = "WIPE PLAYER", embeds = LogArray}), { ['Content-Type'] = 'application/json' })
 	end
+    end
 end
 
 RegisterNetEvent("qb-wipeplayer:server:onjoin", function(source)
@@ -50,17 +50,17 @@ end)
 QBCore.Commands.Add(Config.Commands.addtolist, Lang:t('commands.add'), {}, false, function(source, args)
     local src = source
     if args[1] and tonumber(args[1]) > 0 then
-		local id = tonumber(args[1])
-		if id > 0 then
+	local id = tonumber(args[1])
+	if id > 0 then
             local Player = QBCore.Functions.GetPlayer(id)
             MySQL.Async.fetchAll("SELECT * FROM player_wipes WHERE citizenid = ?", {Player.PlayerData.citizenid}, function(rs)
-				if type(rs) == 'table' and #rs > 0 then
-					TriggerClientEvent('QBCore:Notify', src, Lang:t('info.already_wipe_listed'), "error")
-				else
-					MySQL.Async.execute("INSERT INTO player_wipes (citizenid) VALUES (?)", {Player.PlayerData.citizenid})
-					TriggerClientEvent('QBCore:Notify', src, Lang:t('info.added_to_wipe_list'), "success")
-				end
-			end)
+		if type(rs) == 'table' and #rs > 0 then
+		    TriggerClientEvent('QBCore:Notify', src, Lang:t('info.already_wipe_listed'), "error")
+		else
+		    MySQL.Async.execute("INSERT INTO player_wipes (citizenid) VALUES (?)", {Player.PlayerData.citizenid})
+		    TriggerClientEvent('QBCore:Notify', src, Lang:t('info.added_to_wipe_list'), "success")
+		end
+	    end)
         end
     end
 end, "admin")
@@ -69,8 +69,8 @@ end, "admin")
 QBCore.Commands.Add(Config.Commands.wipeplayer, Lang:t('commands.wipe'), {}, false, function(source, args)
     local src = source
     if args[1] and tonumber(args[1]) > 0 then
-		local id = tonumber(args[1])
-		if id > 0 then
+	local id = tonumber(args[1])
+	if id > 0 then
             local Player = QBCore.Functions.GetPlayer(id)
             TriggerClientEvent('qb-wipeplayer:client:createNewChar', Player.PlayerData.source)
             local fullname = Player.PlayerData.charinfo.firstname .." "..Player.PlayerData.charinfo.lastname
@@ -84,18 +84,18 @@ end, "admin")
 QBCore.Commands.Add(Config.Commands.addcid, Lang:t('commands.addcid'), {}, false, function(source, args)
     local src = source
     if args[1] and tostring(args[1]) then
-		local citizenid = tostring(args[1])
-		if citizenid ~= nil then
+	local citizenid = tostring(args[1])
+	if citizenid ~= nil then
             local Player = QBCore.Functions.GetOfflinePlayerByCitizenId(citizenid)
             local fullname = Player.PlayerData.charinfo.firstname .." "..Player.PlayerData.charinfo.lastname
             MySQL.Async.fetchAll("SELECT * FROM player_wipes WHERE citizenid = ?", {citizenid}, function(rs)
-				if type(rs) == 'table' and #rs > 0 then
-					TriggerClientEvent('QBCore:Notify', src, Lang:t('info.already_wipe_listed', {name= fullname}), "error")
-				else
-					MySQL.Async.execute("INSERT INTO player_wipes (citizenid) VALUES (?)", {citizenid})
-					TriggerClientEvent('QBCore:Notify', src, Lang:t('info.added_to_wipe_list', {name= fullname}), "success")
-				end
-			end)
+		if type(rs) == 'table' and #rs > 0 then
+	            TriggerClientEvent('QBCore:Notify', src, Lang:t('info.already_wipe_listed', {name= fullname}), "error")
+		else
+		    MySQL.Async.execute("INSERT INTO player_wipes (citizenid) VALUES (?)", {citizenid})
+		    TriggerClientEvent('QBCore:Notify', src, Lang:t('info.added_to_wipe_list', {name= fullname}), "success")
+		end
+	    end)
         end
     end
 end, "admin")
